24e9d5dcb3d9d3acd08094f860ba344ca1e2f1e4
Chris Oentojo <c.oentojo@gmail.com>
2013-06-26 21:57:22 -0700

    Changed carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: AMAZON_CONFIG["id"],
-    aws_secret_access_key: AMAZON_CONFIG["secret"]
+    aws_access_key_id: "AKIAJ6DIJ7DDMVTS6H5Q",
+    aws_secret_access_key: "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"
   }
-  config.fog_directory = AMAZON_CONFIG["bucket"]
+  config.fog_directory = "sickpano"
 end
